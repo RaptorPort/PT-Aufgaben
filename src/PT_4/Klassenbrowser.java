315c86a720a7652e1efd99604db518a4d90ec065
Nico <nico.meissen@gmail.com>
2018-11-30 12:45:39 +0100

    menno

---
+++ src/PT_4/Klassenbrowser.java
@@ -0,0 +1,49 @@
+package PT_4;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.TypeVariable;
+import java.util.Scanner;
+
+public class Klassenbrowser {
+
+	public static void main(String[] args) {
+		//Scanner scan = new Scanner(System.in);
+		//String s = scan.nextLine();
+		try {
+			Class<?> c = Class.forName("java.lang.String");
+			System.out.println("class " + c.getName() + " {");
+
+			for (Field f : c.getDeclaredFields()) {
+				TypeVariable<?>[] typeParameters = c.getTypeParameters();
+				if (typeParameters.length < 0) {
+					System.out.print("<");
+					int i = 0;
+			        for (TypeVariable<?> typeParameter : typeParameters) {
+			        	if (i != 0)
+			        		System.out.print(", ");
+			            System.out.print(typeParameter);
+			        }
+			        System.out.print(">");
+				}				
+				System.out.print(Modifier.toString(f.getModifiers()) + " " + f.getName());
+				System.out.println(";");
+				
+				
+			}
+			System.out.println(c.getConstructor(null));
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
